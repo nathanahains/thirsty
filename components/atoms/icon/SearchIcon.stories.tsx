22e11b5215ebd47025a2c72edf37f3e1a9b4c58b
natehains <nathan.hains@modelb.com>
2022-02-08 05:52:31 -0500

    Search icon with respective storybook implementation

---
+++ components/atoms/icon/SearchIcon.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import SearchIcon from "./SearchIcon";
+
+export const searchIcon = <SearchIcon />;
+export default {
+  title: "Icon",
+  component: SearchIcon,
+} as ComponentMeta<typeof SearchIcon>;
