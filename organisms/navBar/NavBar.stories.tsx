87b23ff148afd77599143be881c0ec315bccda7f
natehains <nathan.hains@modelb.com>
2022-02-09 12:07:21 -0500

    Nav bar organism component to display headings/ navigation buttons

---
+++ organisms/navBar/NavBar.stories.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import NavBar from "./NavBar";
+
+export default {
+  title: "Nav Bar",
+  component: NavBar,
+} as ComponentMeta<typeof NavBar>;
+
+const Template: ComponentStory<typeof NavBar> = (args: any) => (
+  <NavBar {...args} />
+);
+
+export const Primary = Template.bind({});
