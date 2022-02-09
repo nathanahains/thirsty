90ba130f06ee62f9832c6a58e15cc939607c5800
natehains <nathan.hains@modelb.com>
2022-02-09 08:05:49 -0500

    Refactors genPastelColor function to increase visability

--- components/particles/genPastelColor.ts
+++ components/particles/genPastelColor.ts
@@ -1,8 +1,13 @@
-const genPastelColor = () => { 
-    return "hsl(" + 360 * Math.random() + ',' +
-               (25 + 70 * Math.random()) + '%,' + 
-               (85 + 10 * Math.random()) + '%)'
-  }
+const genPastelColor = () => {
+  return (
+    "hsl(" +
+    360 * Math.random() +
+    "," +
+    (25 + 70 * Math.random()) +
+    "%," +
+    (75 + 10 * Math.random()) +
+    "%)"
+  );
+};
 
-  export default genPastelColor
-  
+export default genPastelColor;
