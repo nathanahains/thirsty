727f7e19cd53da518cbb91fe5a876318655b7786
natehains <nathan.hains@modelb.com>
2022-02-09 14:39:56 -0500

    Integrates Search bar into index page with proper input change functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,29 @@
-import type { NextPage } from 'next'
-import { useState } from 'react'
-import styled from 'styled-components'
-import SearchBar from '../organisms/searchBar/SearchBar'
+import type { NextPage } from "next";
+import { ChangeEvent, useState } from "react";
+import styled from "styled-components";
+import SearchBar from "../organisms/searchBar/SearchBar";
 
 const Home: NextPage = () => {
-  const [value, setValue] = useState("")
+  const [value, setValue] = useState("");
+  const handleSearchChange = (newValue: string) => {
+    setValue(newValue);
+  };
+  const handleSearchClear = () => {
+    setValue("");
+  };
   return (
     <StyledDiv>
-      <SearchBar value={value}/>
+      <SearchBar
+        onChange={(e: ChangeEvent<HTMLInputElement>): void =>
+          handleSearchChange(e.target.value)
+        }
+        onClick={handleSearchClear}
+        value={value}
+      />
     </StyledDiv>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
-const StyledDiv = styled.div`
-  
-
-`
+const StyledDiv = styled.div``;
