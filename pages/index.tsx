7de9a59e2cd0d736ad434db773b289fdeb76c180
natehains <nathan.hains@modelb.com>
2022-02-09 16:43:22 -0500

    This implements a working index page without a loader to allow the user to fetch drink requests and display them depending on the search params

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,41 @@
 import type { NextPage } from "next";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import styled from "styled-components";
+import { Text } from "../components/atoms/text/Text";
+import ListCell from "../components/molecules/listCell/ListCell";
+import formatDrinkResponse from "../components/particles/formatDrinkResponse";
+import ListContainer from "../organisms/listContainer/ListContainer";
 import SearchBar from "../organisms/searchBar/SearchBar";
+import { fetchDrinks } from "./api/fetchDrinks";
 
 const Home: NextPage = () => {
+  const [data, setData] = useState([]);
   const [value, setValue] = useState("");
+  const [noResults, setNoResults] = useState(false);
+  useEffect(() => {
+    noResults && setNoResults(false);
+    const startGetDrinks = async () => {
+      const [response, ok] = await fetchDrinks(value);
+      if (ok) {
+        if (response.drinks) setData(formatDrinkResponse(response.drinks));
+        else {
+          setData([]);
+          setNoResults(true);
+        }
+      }
+    };
+    value !== "" && startGetDrinks();
+  }, [value]);
+  console.log(data);
   const handleSearchChange = (newValue: string) => {
     setValue(newValue);
   };
   const handleSearchClear = () => {
     setValue("");
+    setData([]);
   };
+  const validData = value && !noResults;
+  const welcome = !validData && !noResults;
   return (
     <StyledDiv>
       <SearchBar
@@ -20,10 +45,27 @@ const Home: NextPage = () => {
         onClick={handleSearchClear}
         value={value}
       />
+      {welcome && (
+        <div className="display-text">
+          <Text content="Thirsty? Find a drink!" />
+        </div>
+      )}
+      
+      {validData && <ListContainer dataSet={data} cell={<ListCell />} />}
+      {noResults && (
+        <div className="display-text">
+          <Text content="No results" />
+        </div>
+      )}
     </StyledDiv>
   );
 };
 
 export default Home;
 
-const StyledDiv = styled.div``;
+const StyledDiv = styled.div`
+  .display-text {
+    margin-top: 20px;
+    text-align: center;
+  }
+`;
