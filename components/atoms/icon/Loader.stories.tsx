f80aac4f60b0e57afebd56c02427b1c2842e2967
natehains <nathan.hains@modelb.com>
2022-02-09 16:52:32 -0500

    Creates a Loader icon atom component that perists a spinning animation with an integrated storybook rendition

---
+++ components/atoms/icon/Loader.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import Loader from "./Loader";
+
+export const loader = <Loader />;
+export default {
+  title: "atoms/Icon",
+  component: Loader,
+} as ComponentMeta<typeof Loader>;
