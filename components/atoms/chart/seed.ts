ea2ab4f432aef2224b8334bb6359c8c117f97c18
natehains <nathan.hains@modelb.com>
2022-02-09 06:12:13 -0500

    Adds additional colors for Pie Chart seed data

--- components/atoms/chart/seed.ts
+++ components/atoms/chart/seed.ts
@@ -1,7 +1,7 @@
 const data = {
-  "1": ["Tequila", "1 1/2 oz ",],
-  "2": ["Triple sec", "1/2 oz "],
-  "3": ["Lime juice", "1 oz "],
+  "1": ["Tequila", "1 1/2 oz ", "red"],
+  "2": ["Triple sec", "1/2 oz ", "blue"],
+  "3": ["Lime juice", "1 oz ", "green"],
   "4": ["Salt", null],
   "5": null,
   "6": null,
