2427f545c7bc757801e82caa1a11672816371625
natehains <nathan.hains@modelb.com>
2022-02-09 21:53:25 -0500

    Adds head tag to display application name

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,15 @@ import type { AppProps } from "next/app";
 import NavBar from "../organisms/navBar/NavBar";
 import AppContext from "../context";
 import { useState } from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [data, setData] = useState([]);
   return (
     <NavBar>
+      <Head>
+        <title>Thirsty</title>
+      </Head>
       <AppContext.Provider
         value={{
           data: data,
